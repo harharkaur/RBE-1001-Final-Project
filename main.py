d4775c4353dd9cca90651803794eff4eca3aaed8
nnewt1 <noahfnewton@gmail.com>
2023-10-09 21:02:48 -0400

    vexifying code

--- main.py
+++ main.py
@@ -10,17 +10,17 @@
 # Change test
 
 # Library imports
-from vex import *
+import vex
 import time
 
-brain = Brain()
-left_motor = Motor(Ports.PORT1, True)
-right_motor = Motor(Ports.PORT2)
-arm_motor = Motor(Ports.PORT6)
-pulley_motor = Motor(Ports.PORT10)
-RED_BALL = Signature (1, 7417, 8677, 8047, -879, -433, -656, 6.1, 0)
-BLUE_BALL = Signature(2, -2235, -1227, -1731, 6473, 9973, 8223, 3,0)
-vision = Vision(Ports.PORT17, 50, RED_BALL, BLUE_BALL)
+brain = vex.Brain()
+left_motor = vex.Motor(vex.Ports.PORT1, True)
+right_motor = vex.Motor(vex.Ports.PORT2)
+arm_motor = vex.Motor(vex.Ports.PORT6)
+pulley_motor = vex.Motor(vex.Ports.PORT10)
+RED_BALL = vex.Signature (1, 7417, 8677, 8047, -879, -433, -656, 6.1, 0)
+BLUE_BALL = vex.Signature(2, -2235, -1227, -1731, 6473, 9973, 8223, 3,0)
+vision = vex.Vision(vex.Ports.PORT17, 50, RED_BALL, BLUE_BALL)
 
 
 drive_speed = 70
@@ -52,15 +52,15 @@ while True:
         while abs(blue_ball.centerX - camera_center_x) > center_x_threshold:
             error = blue_ball.centerX - camera_center_x
             effort = (error * turn_speed) / camera_width
-            left_motor.spin(FORWARD, effort, VelocityUnits.RPM)
-            right_motor.spin(REVERSE, effort, VelocityUnits.RPM)
+            left_motor.spin(vex.FORWARD, effort, vex.VelocityUnits.RPM)
+            right_motor.spin(vex.REVERSE, effort, vex.VelocityUnits.RPM)
             time.sleep(0.1)
             print("applied effort x")
             vision.take_snapshot(BLUE_BALL)
             blue_ball = vision.largest_object()
 
-        left_motor.spin(FORWARD, drive_speed, VelocityUnits.RPM)
-        right_motor.spin(FORWARD, drive_speed, VelocityUnits.RPM)
+        left_motor.spin(vex.FORWARD, drive_speed, vex.VelocityUnits.RPM)
+        right_motor.spin(vex.FORWARD, drive_speed, vex.VelocityUnits.RPM)
 
         while (blue_ball.width < target_width):
             time.sleep(0.25)
@@ -70,8 +70,8 @@ while True:
 
         left_motor.stop()
         right_motor.stop()
-        arm_motor.spin(REVERSE, arm_speed, VelocityUnits.RPM)
-        pulley_motor.spin(FORWARD, 0.70, VelocityUnits.RPM)
+        arm_motor.spin(vex.REVERSE, arm_speed, vex.VelocityUnits.RPM)
+        pulley_motor.spin(vex.FORWARD, 0.70, vex.VelocityUnits.RPM)
         time.sleep(5.2)
         print("arm moved")
         arm_motor.stop()
@@ -79,16 +79,16 @@ while True:
 
         time.sleep(5)
 
-        left_motor.spin(FORWARD, drive_speed, VelocityUnits.RPM)
-        right_motor.spin(FORWARD, drive_speed, VelocityUnits.RPM)
+        left_motor.spin(vex.FORWARD, drive_speed, vex.VelocityUnits.RPM)
+        right_motor.spin(vex.FORWARD, drive_speed, vex.VelocityUnits.RPM)
 
         time.sleep(5)
 
         left_motor.stop()
         right_motor.stop()
 
-        arm_motor.spin(FORWARD, arm_speed, VelocityUnits.RPM)
-        pulley_motor.spin(REVERSE, 0.70, VelocityUnits.RPM)
+        arm_motor.spin(vex.FORWARD, arm_speed, vex.VelocityUnits.RPM)
+        pulley_motor.spin(vex.REVERSE, 0.70, vex.VelocityUnits.RPM)
         time.sleep(5.2)
         print("arm moved up")
         arm_motor.stop()
@@ -96,8 +96,8 @@ while True:
         break
 
     else:
-        left_motor.spin(REVERSE, turn_speed, VelocityUnits.RPM)
-        right_motor.spin(FORWARD, turn_speed, VelocityUnits.RPM)
+        left_motor.spin(vex.REVERSE, turn_speed, vex.VelocityUnits.RPM)
+        right_motor.spin(vex.FORWARD, turn_speed, vex.VelocityUnits.RPM)
         time.sleep(1)
         print("obj not detected")
         left_motor.stop()
